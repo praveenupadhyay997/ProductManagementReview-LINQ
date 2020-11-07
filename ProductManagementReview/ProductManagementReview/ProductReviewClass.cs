4511f8f7856b52a16a4dac7b7182268fb8c9ddab
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-08 05:07:42 +0530

    [Praveen] Add: Created a product review list with 25 default values.

---
+++ ProductManagementReview/ProductManagementReview/ProductReviewClass.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProductReviewRepository.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ProductManagementReview
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// Class defining the review attributes inside the customer review
+    /// </summary>
+    public class ProductReviewClass
+    {
+        /// <summary>
+        /// Member Data Attribute of the review to be fed by users
+        /// </summary>
+        public int productId;
+        public int userId;
+        public int rating;
+        public string review;
+        /// <summary>
+        /// PArameterised constructor initialising the instance of the Product Review Class
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="userId"></param>
+        /// <param name="rating"></param>
+        /// <param name="review"></param>
+        public ProductReviewClass(int productId, int userId, int rating, string review)
+        {
+            this.productId = productId;
+            this.userId = userId;
+            this.rating = rating;
+            this.review = review;
+        }
+    }
+}
---
+++ ProductManagementReview/ProductManagementReview/Program.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ProductManagementReview
+{
+    using System;
+    class Program
+    {
+        /// <summary>
+        /// Main Driver function
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine("Welcome to Product Review Program");
+            Console.WriteLine("===================================");
+            /// Creating the instance of the product review repository
+            ProductReviewRepository productReview = new ProductReviewRepository();
+            /// Printing the details stored inside the reviews
+            productReview.DisplayTheProductReviewDetails();
+        }
+    }
+}
